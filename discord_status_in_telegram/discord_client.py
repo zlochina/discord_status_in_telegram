7cae1d68880f25986df56afad40dcd06c579984f
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-03 20:51:24 +0200

    Added connection event to DiscordClient

--- discord_status_in_telegram/discord_client.py
+++ discord_status_in_telegram/discord_client.py
@@ -6,6 +6,18 @@ class DiscordClient:
         self.client = discord.Client(intents=discord.Intents.default())
         self.token = token
         self.guild_id = int(guild_id)
+        self.setup_events()
+
+    def setup_events(self):
+        @self.client.event
+        async def on_ready(self):
+            print(f"{self.client.user.name} has connected to Discord!")
+
+    async def start(self):
+        await self.client.start(self.token)
+
+    async def close(self):
+        await self.client.close()
 
     async def get_voice_channels_status(self):
         await self.client.login(self.token)
