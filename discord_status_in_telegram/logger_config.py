706c48927d2f5271b8880a7f3192e12852314858
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-03 21:28:09 +0200

    Updated logger_config.py

---
+++ discord_status_in_telegram/logger_config.py
@@ -0,0 +1,28 @@
+import logging
+
+
+def setup_logger(name):
+    # Create a custom logger
+    logger = logging.getLogger(name)
+
+    # Set level
+    logger.setLevel(logging.INFO)
+
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler("app.log")
+    c_handler.setLevel(logging.INFO)
+    f_handler.setLevel(logging.INFO)
+
+    # Create formatters and add it to handlers
+    log_format = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    c_handler.setFormatter(log_format)
+    f_handler.setFormatter(log_format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    return logger
