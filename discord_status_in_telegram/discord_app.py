02a0ce6d0dc3b8eb8dea9fe178b8130b149b4f45
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-04 18:20:18 +0200

    Moved DiscordApp to its own file

---
+++ discord_status_in_telegram/discord_app.py
@@ -0,0 +1,30 @@
+import discord
+from flask import Flask, redirect, request
+
+
+class DiscordApp:
+    app = Flask(__name__)
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def handle_authorization_code(self, code):
+        # Exchange the authorization code for an access token
+        self.access_token = discord.utils.oauth_complete(
+            self.client_id, self.client_secret, code
+        )
+
+    @app.route("/discord/callback")
+    def discord_callback(self):
+        # Get the authorization code from the query parameters
+        code = request.args.get("code")
+
+        # Handle the authorization code and exchange it for an access token
+        self.handle_authorization_code(code)
+
+        # For now, let's just redirect back to the homepage
+        return redirect("/")
+
+    def run_applicaiton(self):
+        self.app.run(debug=True, port=8000)
--- discord_status_in_telegram/discord_client.py
+++ discord_status_in_telegram/discord_client.py
@@ -1,5 +1,4 @@
 import discord
-from flask import Flask, redirect, request
 
 
 class DiscordClient:
@@ -37,31 +36,3 @@ class DiscordClient:
 
         await self.client.close()
         return status
-
-
-class DiscordApp:
-    app = Flask(__name__)
-
-    def __init__(self, client_id, client_secret):
-        self.client_id = client_id
-        self.client_secret = client_secret
-
-    def handle_authorization_code(self, code):
-        # Exchange the authorization code for an access token
-        self.access_token = discord.utils.oauth_complete(
-            self.client_id, self.client_secret, code
-        )
-
-    @app.route("/discord/callback")
-    def discord_callback(self):
-        # Get the authorization code from the query parameters
-        code = request.args.get("code")
-
-        # Handle the authorization code and exchange it for an access token
-        self.handle_authorization_code(code)
-
-        # For now, let's just redirect back to the homepage
-        return redirect("/")
-
-    def run_applicaiton(self):
-        self.app.run(debug=True, port=8000)
