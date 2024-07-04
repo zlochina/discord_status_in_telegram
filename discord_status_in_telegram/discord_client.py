d892f0549d8310979534f21ea41c92844a326064
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-04 20:35:55 +0200

    Updated discord_client.py with logging

--- discord_status_in_telegram/discord_client.py
+++ discord_status_in_telegram/discord_client.py
@@ -1,8 +1,12 @@
 import discord
+from logger_config import setup_logger
+
+logger = setup_logger(__name__)
 
 
 class DiscordClient:
     def __init__(self, token, guild_id):
+        logger.info("Initializing Discord Client")
         intents = discord.Intents.default()
         intents.members = True
         self.client = discord.Client(intents=intents)
@@ -11,17 +15,22 @@ class DiscordClient:
         self.setup_events()
 
     def setup_events(self):
+        logger.debug("Setting up Discord events")
+
         @self.client.event
         async def on_ready(self):
-            print(f"{self.client.user.name} has connected to Discord!")
+            logger.info(f"{self.client.user.name} has connected to Discord!")
 
     async def start(self):
+        logger.info("Starting Discord Client")
         await self.client.start(self.token)
 
     async def close(self):
+        logger.info("Closing Discord Client")
         await self.client.close()
 
     async def get_voice_channels_status(self):
+        logger.info("Getting voice channels status")
         await self.client.login(self.token)
         guild = self.client.get_guild(self.guild_id)
 
