68a42aa5e60ccf39ebac4b54648e9fe09e47c987
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-07-04 18:07:09 +0200

    Added DiscordApp class to discord_client.py to handle and retrieve access_token

--- discord_status_in_telegram/discord_client.py
+++ discord_status_in_telegram/discord_client.py
@@ -1,9 +1,12 @@
 import discord
+from flask import Flask, redirect, request
 
 
 class DiscordClient:
     def __init__(self, token, guild_id):
-        self.client = discord.Client(intents=discord.Intents.default())
+        intents = discord.Intents.default()
+        intents.members = True
+        self.client = discord.Client(intents=intents)
         self.token = token
         self.guild_id = int(guild_id)
         self.setup_events()
@@ -34,3 +37,31 @@ class DiscordClient:
 
         await self.client.close()
         return status
+
+
+class DiscordApp:
+    app = Flask(__name__)
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def handle_authorization_code(self, code):
+        # Exchange the authorization code for an access token
+        self.access_token = discord.utils.oauth_complete(
+            self.client_id, self.client_secret, code
+        )
+
+    @app.route("/discord/callback")
+    def discord_callback(self):
+        # Get the authorization code from the query parameters
+        code = request.args.get("code")
+
+        # Handle the authorization code and exchange it for an access token
+        self.handle_authorization_code(code)
+
+        # For now, let's just redirect back to the homepage
+        return redirect("/")
+
+    def run_applicaiton(self):
+        self.app.run(debug=True, port=8000)
